555a4369b2c201a1f27b203f9a2f12c18735a879
Pedro Dorini <pedro.dorini@hotmail.com>
2019-01-27 16:12:58 -0200

    pass themes to context

--- src/context/Theme.js
+++ src/context/Theme.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const ThemeContext = React.createContext(false)
 
+const themes = {
+  dark: { backgroundColor: '#555555', color: '#FFFFFF' },
+  light: { backgroundColor: '#F5F5F5', color: '#000000' }
+}
+
 export const ThemeProvider = ({ children, value }) =>
-  <ThemeContext.Provider value={value}>
+  <ThemeContext.Provider value={({ themes: themes, ...value })}>
     { children }
   </ThemeContext.Provider>
 
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { HomeContainer, Container } from './style.js'
-import { ThemeProvider } from '@context/Theme'
+import { ThemeProvider, ThemeConsumer } from '@context/Theme'
 import Button from '@components/Button'
 import Header from '@components/Header'
 
@@ -8,11 +8,7 @@ class Home extends Component {
   constructor() {
     super()
     this.state = {
-      theme: 'light',
-      themes: {
-        dark: { backgroundColor: '#555555', color: '#FFFFFF' },
-        light: { backgroundColor: '#F5F5F5', color: '#000000' }
-      }
+      theme: 'light'
     }
   }
 
@@ -21,15 +17,19 @@ class Home extends Component {
   }
 
   render () {
-    const { theme, themes } = this.state
+    const { theme } = this.state
     return (
-      <ThemeProvider value={({ themes, theme, toggleTheme: this.toggleTheme })}>
-        <HomeContainer backgroundColor={themes[theme].backgroundColor} color={themes[theme].color}>
-          <Container>
-            <Header/>
-            <Button/>
-          </Container>
-        </HomeContainer>
+      <ThemeProvider value={({ theme, toggleTheme: this.toggleTheme })}>
+        <ThemeConsumer>
+          { ({ themes }) => (
+            <HomeContainer backgroundColor={themes[theme].backgroundColor} color={themes[theme].color}>
+              <Container>
+                <Header/>
+                <Button/>
+              </Container>
+            </HomeContainer>
+          )}
+        </ThemeConsumer>
       </ThemeProvider>
     )
   }
